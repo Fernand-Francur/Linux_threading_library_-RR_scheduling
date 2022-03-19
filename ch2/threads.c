c8c5f45d140d37d77a4630f09470a143d0e339f8
Timothy Borunov <timobohr@bu.edu>
2022-03-19 13:42:22 -0400

    Functions add

--- ch2/threads.c
+++ ch2/threads.c
@@ -328,6 +328,45 @@ int pthread_create(
     return -1;
 }
 
+int pthread_barrier_init(   pthread_barrier_t *restrict barrier,
+                            const pthread_barrierattr_t *restrict attr,
+                            unsigned count)
+{
+    return 0;
+}
+
+int pthread_barrier_destroy(pthread_barrier_t *barrier)
+{
+    return 0;
+}
+
+int pthread_barrier_wait(pthread_barrier_t *barrier)
+{
+    return 0;
+}
+
+int pthread_mutex_lock(pthread_mutex_t *mutex)
+{
+    return 0;
+}
+
+int pthread_mutex_unlock(pthread_mutex_t *mutex)
+{
+    return 0;
+}
+
+int pthread_mutex_init(
+    pthread_mutex_t *restrict mutex,
+    const pthread_mutexattr_t *restrict attr)
+{
+    return 0;
+}
+
+int pthread_mutex_destroy(
+    pthread_mutex_t *mutex)
+{
+    return 0;
+}
 
 
 pthread_t pthread_self(void)
